68d0c6e831183079ba7a52e0ea6604a28abeb343
vamosviendo <htejedor@gmail.com>
2021-03-16 20:44:04 -0300

    FT chequea encabezado y posibilidad de ingresar un item a la lista de tareas

--- functional_tests.py
+++ functional_tests.py
@@ -1,5 +1,8 @@
-from selenium import webdriver
+import time
 import unittest
+
+from selenium import webdriver
+from selenium.webdriver.common.keys import Keys
 
 
 class NewVisitorTest(unittest.TestCase):
@@ -13,7 +16,27 @@ class NewVisitorTest(unittest.TestCase):
     def test_puede_comenzar_una_lista_y_recuperarla_mas_tarde(self):
         self.browser.get('http://localhost:8000')
 
+        # El título y el encabezado de la página mencionan listas de tareas
         self.assertIn('tareas', self.browser.title)
+        texto_encabezado = self.browser.find_element_by_tag_name('h1').text
+        self.assertIn('tareas', texto_encabezado)
+
+        # Inmediatamente después hay una invitación a ingresar tareas
+        inputbox = self.browser.find_element_by_id('id_item_nuevo')
+        self.assertEqual(
+            inputbox.get_attribute('placeholder'),
+            'Ingrese una tarea'
+        )
+        inputbox.send_keys('Hacer las compras')
+        inputbox.send_keys(Keys.ENTER)
+        time.sleep(1)
+
+        tabla = self.browser.find_element_by_id('id_tabla_lista')
+        filas = tabla.find_elements_by_tag_name('tr')
+        self.assertTrue(
+            any(fila.text == '1: Hacer las compras' for fila in filas)
+        )
+
         self.fail('Terminar el test!')
 
 
