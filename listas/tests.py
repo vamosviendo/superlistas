c33cdcd787512ece0f01bb6b44f4ce0501755cfa
vamosviendo <htejedor@gmail.com>
2021-03-16 20:21:08 -0300

    Primer UT y mapeado de url, vista dummy

--- listas/tests.py
+++ listas/tests.py
@@ -1,6 +1,9 @@
+from django.urls import resolve
 from django.test import TestCase
+from listas.views import home_page
 
-class TestDeHumo(TestCase):
+class HomePageTest(TestCase):
 
-    def test_matem(self):
-        self.assertEqual(1+1, 3)
+    def test_url_raiz_resuelve_a_vista_home_page(self):
+        found = resolve('/')
+        self.assertEqual(found.func, home_page)
