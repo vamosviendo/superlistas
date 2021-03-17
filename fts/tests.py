da0ede049553a4fc34bd90bdcf7b47f1b3ea348c
vamosviendo <htejedor@gmail.com>
2021-03-17 19:31:34 -0300

    Primeros pasos de un FT para distribución y estilo

--- fts/tests.py
+++ fts/tests.py
@@ -95,3 +95,25 @@ class NewVisitorTest(LiveServerTestCase):
         texto_pag = self.browser.find_element_by_tag_name('body').text
         self.assertNotIn('Hacer las compras', texto_pag)
         self.assertIn('Cortar el pasto', texto_pag)
+
+    def test_distribucion_y_estilo(self):
+        self.browser.get(self.live_server_url)
+        self.browser.set_window_size(1024, 768)
+
+        # El campo inputbox debe estar centrado en la página
+        inputbox = self.browser.find_element_by_id('id_item_nuevo')
+        self.assertAlmostEqual(
+            inputbox.location['x'] + inputbox.size['width'] / 2,
+            512,
+            delta=10
+        )
+
+        inputbox.send_keys('testing')
+        inputbox.send_keys(Keys.ENTER)
+        self.esperar_fila_en_tabla_lista('1: testing')
+        inputbox = self.browser.find_element_by_id('id_item_nuevo')
+        self.assertAlmostEqual(
+            inputbox.location['x'] + inputbox.size['width'] / 2,
+            512,
+            delta=10
+        )
