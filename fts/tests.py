73614ffc1a7a03fc94c20eb038eeaebeaabc009a
vamosviendo <htejedor@gmail.com>
2021-03-17 16:44:43 -0300

    Retiradas esperas explícitas y agregado FT para más de un usuario

--- fts/tests.py
+++ fts/tests.py
@@ -3,7 +3,11 @@ import time
 from django.test import LiveServerTestCase
 
 from selenium import webdriver
+from selenium.common.exceptions import WebDriverException
 from selenium.webdriver.common.keys import Keys
+
+
+MAX_WAIT = 10
 
 
 class NewVisitorTest(LiveServerTestCase):
@@ -14,10 +18,18 @@ class NewVisitorTest(LiveServerTestCase):
     def tearDown(self):
         self.browser.quit()
 
-    def buscar_fila_en_tabla_lista(self, texto_fila):
-        tabla = self.browser.find_element_by_id('id_tabla_lista')
-        filas = tabla.find_elements_by_tag_name('tr')
-        self.assertIn(texto_fila, [fila.text for fila in filas])
+    def esperar_fila_en_tabla_lista(self, texto_fila):
+        inicio = time.time()
+        while True:
+            try:
+                tabla = self.browser.find_element_by_id('id_tabla_lista')
+                filas = tabla.find_elements_by_tag_name('tr')
+                self.assertIn(texto_fila, [fila.text for fila in filas])
+                return
+            except (AssertionError, WebDriverException) as e:
+                if time.time() - inicio > MAX_WAIT:
+                    raise e
+                time.sleep(0.5)
 
     def test_puede_comenzar_una_lista_y_recuperarla_mas_tarde(self):
         self.browser.get(self.live_server_url)
@@ -35,16 +47,51 @@ class NewVisitorTest(LiveServerTestCase):
         )
         inputbox.send_keys('Hacer las compras')
         inputbox.send_keys(Keys.ENTER)
-        time.sleep(1)
 
-        self.buscar_fila_en_tabla_lista('1: Hacer las compras')
+        self.esperar_fila_en_tabla_lista('1: Hacer las compras')
 
         inputbox = self.browser.find_element_by_id('id_item_nuevo')
         inputbox.send_keys('Guardar las vituallas')
         inputbox.send_keys(Keys.ENTER)
-        time.sleep(1)
 
-        self.buscar_fila_en_tabla_lista('1: Hacer las compras')
-        self.buscar_fila_en_tabla_lista('2: Guardar las vituallas')
+        self.esperar_fila_en_tabla_lista('1: Hacer las compras')
+        self.esperar_fila_en_tabla_lista('2: Guardar las vituallas')
 
-        self.fail('Terminar el test!')
+    def test_distintos_usuarios_pueden_empezar_listas_en_distintas_urls(self):
+
+        # La usuaria Edith inicia una lista
+        self.browser.get(self.live_server_url)
+        inputbox = self.browser.find_element_by_id('id_item_nuevo')
+        inputbox.send_keys('Hacer las compras')
+        inputbox.send_keys(Keys.ENTER)
+        self.esperar_fila_en_tabla_lista('1: Hacer las compras')
+
+        url_lista_edith = self.browser.current_url
+        self.assertRegex(url_lista_edith, '/listas/.+')
+
+        # La usuaria Edith abandona el sitio. El usuario Francis ingresa al
+        # sitio.
+        self.browser.quit()
+        self.browser = webdriver.Firefox()
+        self.browser.get(self.live_server_url)
+
+        # No hay rastros de la lista de Edith
+        texto_pag = self.browser.find_elements_by_tag_name('body').text
+        self.assertNotIn('Hacer las compras', texto_pag)
+        self.assertNotIn('vituallas', texto_pag)
+
+        # Francis da inicio a una nueva lista
+        inputbox = self.browser.find_element_by_id('id_item_nuevo')
+        inputbox.send_keys('Cortar el pasto')
+        inputbox.send_keys(Keys.ENTER)
+        self.esperar_fila_en_tabla_lista('1: Cortar el pasto')
+
+        # Francis tiene su propia URL única
+        url_lista_francis = self.browser.current_url
+        self.assertRegex(url_lista_francis, '/listas/.+')
+        self.assertNotEqual(url_lista_francis, url_lista_edith)
+
+        # Una vez más, no hay rastros de la lista de Edith
+        texto_pag = self.browser.find_element_by_tag_name('body').text
+        self.assertNotIn('Hacer las compras', texto_pag)
+        self.assertIn('Cortar el pasto', texto_pag)
