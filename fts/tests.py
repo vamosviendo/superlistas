c5f158c8fb85d1c27f85413fe278d6e1f7b232d7
vamosviendo <htejedor@gmail.com>
2021-03-17 20:14:36 -0300

    Agregado Bootstrap para mejoras de diseño

--- fts/tests.py
+++ fts/tests.py
@@ -1,6 +1,6 @@
 import time
 
-from django.test import LiveServerTestCase
+from django.contrib.staticfiles.testing import StaticLiveServerTestCase
 
 from selenium import webdriver
 from selenium.common.exceptions import WebDriverException
@@ -10,7 +10,7 @@ from selenium.webdriver.common.keys import Keys
 MAX_WAIT = 2
 
 
-class NewVisitorTest(LiveServerTestCase):
+class NewVisitorTest(StaticLiveServerTestCase):
 
     def setUp(self):
         self.browser = webdriver.Firefox()
