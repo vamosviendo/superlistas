932ddf671e890da5f2bc4b2bd8ff1c4e879b6e84
vamosviendo <htejedor@gmail.com>
2021-03-17 08:29:59 -0300

    Model para Items de listas y migración asociada

--- listas/tests.py
+++ listas/tests.py
@@ -1,5 +1,7 @@
 from django.urls import resolve
 from django.test import TestCase
+
+from listas.models import Item
 from listas.views import home_page
 
 
@@ -17,3 +19,24 @@ class HomePageTest(TestCase):
         response = self.client.post('/', data={'texto_item': 'Nuevo item'})
         self.assertIn('Nuevo item', response.content.decode())
         self.assertTemplateUsed(response, 'home.html')
+
+
+class ItemModelTest(TestCase):
+
+    def test_salvar_y_recuperar_items(self):
+        primer_item = Item()
+        primer_item.text = 'El primer item de la lista'
+        primer_item.save()
+
+        segundo_item = Item()
+        segundo_item.text = 'Segundo item'
+        segundo_item.save()
+
+        items_guardados = Item.objects.all()
+        self.assertEqual(items_guardados.count(), 2)
+
+        primer_item_guardado = items_guardados[0]
+        segundo_item_guardado = items_guardados[1]
+        self.assertEqual(
+            primer_item_guardado.text, 'El primer item de la lista')
+        self.assertEqual(segundo_item_guardado.text, 'Segundo item')
