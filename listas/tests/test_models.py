f7181ee6e5b126531f54ecf4a58ade3386a1f527
vamosviendo <htejedor@gmail.com>
2021-03-20 18:17:58 -0300

    UTs divididos en dos archivos.

---
+++ listas/tests/test_models.py
@@ -0,0 +1,33 @@
+from django.test import TestCase
+
+from listas.models import Item, Lista
+
+
+class ListaAndItemModelTest(TestCase):
+
+    def test_salvar_y_recuperar_items(self):
+        lista = Lista()
+        lista.save()
+        primer_item = Item()
+        primer_item.texto = 'El primer item de la lista'
+        primer_item.lista = lista
+        primer_item.save()
+
+        segundo_item = Item()
+        segundo_item.texto = 'Segundo item'
+        segundo_item.lista = lista
+        segundo_item.save()
+
+        lista_guardada = Lista.objects.first()
+        self.assertEqual(lista_guardada, lista)
+
+        items_guardados = Item.objects.all()
+        self.assertEqual(items_guardados.count(), 2)
+
+        primer_item_guardado = items_guardados[0]
+        segundo_item_guardado = items_guardados[1]
+        self.assertEqual(
+            primer_item_guardado.texto, 'El primer item de la lista')
+        self.assertEqual(primer_item_guardado.lista, lista)
+        self.assertEqual(segundo_item_guardado.texto, 'Segundo item')
+        self.assertEqual(segundo_item_guardado.lista, lista)
