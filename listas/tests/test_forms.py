60194ed5d42319d7e9e9e408d1fd02e5386543a2
vamosviendo <htejedor@gmail.com>
2021-03-21 10:24:20 -0300

    Form ItemForm se encarga de guardar los items

--- listas/tests/test_forms.py
+++ listas/tests/test_forms.py
@@ -1,6 +1,7 @@
 from django.test import TestCase
 
 from listas.forms import ERROR_ITEM_VACIO, ItemForm
+from listas.models import Lista, Item
 
 
 class ItemFormTest(TestCase):
@@ -14,3 +15,11 @@ class ItemFormTest(TestCase):
         form = ItemForm(data={'text': ''})
         self.assertFalse(form.is_valid())
         self.assertEqual(form.errors['texto'], [ERROR_ITEM_VACIO])
+
+    def test_form_save_maneja_el_guardado_en_una_lista(self):
+        lista = Lista.objects.create()
+        form = ItemForm(data={'texto': 'hacerme'})
+        nuevo_item = form.save(en_lista=lista)
+        self.assertEqual(nuevo_item, Item.objects.first())
+        self.assertEqual(nuevo_item.texto, 'hacerme')
+        self.assertEqual(nuevo_item.lista, lista)
--- listas/views.py
+++ listas/views.py
@@ -13,7 +13,7 @@ def nueva_lista(request):
     form = ItemForm(data=request.POST)
     if form.is_valid():
         lista = Lista.objects.create()
-        Item.objects.create(texto=request.POST['texto'], lista=lista)
+        form.save(en_lista=lista)
         return redirect(lista)
     else:
         return render(request, 'home.html', {'form': form})
@@ -26,6 +26,6 @@ def view_lista(request, lista_id):
     if request.method == 'POST':
         form = ItemForm(data=request.POST)
         if form.is_valid():
-            Item.objects.create(texto=request.POST['texto'], lista=lista)
+            form.save(en_lista=lista)
             return redirect(lista)
     return render(request, 'lista.html', {'lista': lista, 'form': form})
