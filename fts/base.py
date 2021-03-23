3aadbda37ac177c69667a04f2d9002084e18c970
vamosviendo <htejedor@gmail.com>
2021-03-23 08:11:13 -0300

    FTs refactoring: decorator espera

--- fts/base.py
+++ fts/base.py
@@ -10,6 +10,21 @@ from selenium.common.exceptions import WebDriverException
 MAX_WAIT = 10
 
 
+def espera(fn):
+
+    def fn_modificada(*args, **kwargs):
+        inicio = time.time()
+        while True:
+            try:
+                return fn(*args, **kwargs)
+            except (AssertionError, WebDriverException) as e:
+                if time.time() - inicio > MAX_WAIT:
+                    raise e
+                time.sleep(0.5)
+
+    return fn_modificada
+
+
 class FunctionalTest(StaticLiveServerTestCase):
 
     def setUp(self):
@@ -21,40 +36,19 @@ class FunctionalTest(StaticLiveServerTestCase):
     def tearDown(self):
         self.browser.quit()
 
+    @espera
     def esperar_a(self, fn):
-        inicio = time.time()
-        while True:
-            try:
-                return fn()
-            except (AssertionError, WebDriverException) as e:
-                if time.time() - inicio > MAX_WAIT:
-                    raise e
-                time.sleep(0.5)
+        return fn()
 
-    def esperar_fila_en_tabla_lista(self, texto_fila):
-        inicio = time.time()
-        while True:
-            try:
-                tabla = self.browser.find_element_by_id('id_tabla_lista')
-                filas = tabla.find_elements_by_tag_name('tr')
-                self.assertIn(texto_fila, [fila.text for fila in filas])
-                return
-            except (AssertionError, WebDriverException) as e:
-                if time.time() - inicio > MAX_WAIT:
-                    raise e
-                time.sleep(0.5)
-
+    @espera
     def esperar_ingreso(self, email):
-        self.esperar_a(
-            lambda: self.browser.find_element_by_link_text('Salir')
-        )
+        self.browser.find_element_by_link_text('Salir')
         navbar = self.browser.find_element_by_css_selector('.navbar')
         self.assertIn(email, navbar.text)
 
+    @espera
     def esperar_salida(self, email):
-        self.esperar_a(
-            lambda: self.browser.find_element_by_name('email')
-        )
+        self.browser.find_element_by_name('email')
         navbar = self.browser.find_element_by_css_selector('.navbar')
         self.assertNotIn(email, navbar.text)
 
